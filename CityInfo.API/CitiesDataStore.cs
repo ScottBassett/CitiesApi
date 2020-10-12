f445494a2898ac847810ee55fb17f876e5a168d5
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-10-12 15:56:35 +0100

    Added points of interest dto. Each city now has a list of points of interests

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -21,19 +21,52 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with that big park."
+                    Description = "The one with that big park.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto() {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States." },
+                        new PointOfInterestDto() {
+                            Id = 2,
+                            Name = "Empire State Building",
+                            Description = "A 102-story skyscraper located in Midtown Manhattan." },
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Swansea",
-                    Description = "The one with the beach."
+                    Description = "The one with the beach.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto() {
+                            Id = 3,
+                            Name = "Mumbles Lighthouse",
+                            Description = "A lighthouse that sits at the end of the mumbles coast." },
+                        new PointOfInterestDto() {
+                            Id = 4,
+                            Name = "Liberty Stadium",
+                            Description = "Home to Swansea City FC and the Ospreys." },
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Gimcheon",
-                    Description = "The one with all the best things ever."
+                    Description = "The one with all the best things ever.",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto() {
+                            Id = 5,
+                            Name = "Moms Touch",
+                            Description = "The best chicken burgers you will ever taste in your life." },
+                        new PointOfInterestDto() {
+                            Id = 6,
+                            Name = "Gimcheon University",
+                            Description = "Home of the best  festivals in Gimcheon, see your favourite K-Pop stars every year!" },
+                    }
                 },
             };
         }
