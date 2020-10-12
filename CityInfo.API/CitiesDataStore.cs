9b26861c526dae18dc0ae136728008d9e89d5ee3
ScottBassett <57405246+ScottBassett@users.noreply.github.com>
2020-10-12 14:49:16 +0100

    Created a data store which uses a Dto model to poulate a list of cities. Used this to make api call

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+
+        public CitiesDataStore()
+        {
+            // init dummy data
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park."
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Swansea",
+                    Description = "The one with the beach."
+                },
+                new CityDto()
+                {
+                    Id = 3,
+                    Name = "Gimcheon",
+                    Description = "The one with all the best things ever."
+                },
+            };
+        }
+    }
+}
--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -14,12 +14,7 @@ namespace CityInfo.API.Controllers
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(
-                new List<object>()
-                {
-                    new { id = 1, Name = "New York City" },
-                    new { id = 2, Name = "Swansea" }
-                });
+            return new JsonResult(CitiesDataStore.Current.Cities);
         }
     }
 }
